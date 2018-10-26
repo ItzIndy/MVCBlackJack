59c978c5db43e3ab271ed7a961af477f3c67794d
Indy Van Canegem <indy.vancanegem@student.hogent.be>
2018-10-26 10:17:14 +0200

    Exercise

---
+++ MVCBlackJackGame/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using BlackJackGame.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MVCBlackJackGame.Controllers {
+    public class HomeController : Controller {
+        private BlackJack _blackJack;
+        public IActionResult Index() {
+            _blackJack = new BlackJack();
+            HttpContext.Session.SetString("BlackJack", JsonConvert.SerializeObject(_blackJack));
+            return View(_blackJack);
+        }
+
+      
+    }
+}
